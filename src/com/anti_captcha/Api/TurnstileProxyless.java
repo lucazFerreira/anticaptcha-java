c1ce4763a626c0a9ca837d4d0f142ff1296c9417
Anti Admin <robot@antigate.com>
2022-11-02 14:56:19 +0700

    Added Turnstile support

---
+++ src/com/anti_captcha/Api/TurnstileProxyless.java
@@ -0,0 +1,46 @@
+package com.anti_captcha.Api;
+
+import com.anti_captcha.AnticaptchaBase;
+import com.anti_captcha.ApiResponse.TaskResultResponse;
+import com.anti_captcha.Helper.DebugHelper;
+import com.anti_captcha.IAnticaptchaTaskProtocol;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+
+public class TurnstileProxyless extends AnticaptchaBase implements IAnticaptchaTaskProtocol {
+    private URL websiteUrl;
+    private String websiteKey;
+    private String websiteSToken;
+
+    public void setWebsiteUrl(URL websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
+
+    public void setWebsiteKey(String websiteKey) {
+        this.websiteKey = websiteKey;
+    }
+
+    @Override
+    public JSONObject getPostData() {
+        JSONObject postData = new JSONObject();
+
+        try {
+            postData.put("type", "TurnstileTaskProxyless");
+            postData.put("websiteURL", websiteUrl.toString());
+            postData.put("websiteKey", websiteKey);
+        } catch (JSONException e) {
+            DebugHelper.out("JSON compilation error: " + e.getMessage(), DebugHelper.Type.ERROR);
+
+            return null;
+        }
+
+        return postData;
+    }
+
+    @Override
+    public TaskResultResponse.SolutionData getTaskSolution() {
+        return taskInfo.getSolution();
+    }
+}
