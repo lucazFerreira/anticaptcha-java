742ddd89205326b1623c02a5f798c755c2a192e1
Anti Admin <robot@antigate.com>
2023-04-18 14:56:44 +0700

    Turnstile action support

--- src/com/anti_captcha/Api/TurnstileProxyless.java
+++ src/com/anti_captcha/Api/TurnstileProxyless.java
@@ -12,7 +12,7 @@ import java.net.URL;
 public class TurnstileProxyless extends AnticaptchaBase implements IAnticaptchaTaskProtocol {
     private URL websiteUrl;
     private String websiteKey;
-    private String websiteSToken;
+    private String action;
 
     public void setWebsiteUrl(URL websiteUrl) {
         this.websiteUrl = websiteUrl;
@@ -20,6 +20,10 @@ public class TurnstileProxyless extends AnticaptchaBase implements IAnticaptchaT
 
     public void setWebsiteKey(String websiteKey) {
         this.websiteKey = websiteKey;
+    }
+
+    public void setAction(String websiteKey) {
+        this.action = action;
     }
 
     @Override
@@ -30,6 +34,7 @@ public class TurnstileProxyless extends AnticaptchaBase implements IAnticaptchaT
             postData.put("type", "TurnstileTaskProxyless");
             postData.put("websiteURL", websiteUrl.toString());
             postData.put("websiteKey", websiteKey);
+            postData.put("action", action);
         } catch (JSONException e) {
             DebugHelper.out("JSON compilation error: " + e.getMessage(), DebugHelper.Type.ERROR);
 
